7343900c9dcd5c91fea194a0e8a6edf4f4745d57
MIRsky <miralexsky@gmail.ru>
2018-12-11 16:07:38 +0400

    load_file

--- learn/php/load_File.php
+++ learn/php/load_File.php
@@ -8,7 +8,7 @@ foreach ($_FILES["pictures"]["error"] as $key => $error) {
     if ($error == UPLOAD_ERR_OK) {
         $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
         // basename() может предотвратить атаку на файловую систему;
-        // может быть целесообразным дополнительно проверить имя файла
+        // может быть целесообразным дополнительно проверить имя файла  
         $name = basename($_FILES["pictures"]["name"][$key]);
         move_uploaded_file($tmp_name, "$uploads_dir/$name");
     }
